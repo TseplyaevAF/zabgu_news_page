14da57502bc7402569e6dcc987193596f8d5a97e
TseplyaevAF <tseplyaeff.alex@yandex.ru>
2023-03-07 15:56:27 +0900

    Update CsvWriter

--- src/CsvWriter.java
+++ src/CsvWriter.java
@@ -9,11 +9,20 @@ public class CsvWriter {
         try (FileWriter fw = new FileWriter(csvOutputFile, true)) {
             for (String[] data : dataLines) {
                 for (int i = 0; i < data.length; i++) {
-                    fw.write(data[i] + ";");
+                    fw.write(escapeSpecialCharacters(data[i]) + ";");
                 }
                 fw.write("\n");
             }
             fw.close();
         }
+    }
+
+    private String escapeSpecialCharacters(String data) {
+        String escapedData = data.replaceAll("\\R", " ");
+        if (data.contains(",") || data.contains("\"") || data.contains("'")) {
+            data = data.replace("\"", "\"\"");
+            escapedData = "\"" + data + "\"";
+        }
+        return escapedData;
     }
 }
