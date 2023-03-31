29e011772a9247a5a2bf5f99c1932c25525abee6
TseplyaevAF <tseplyaeff.alex@yandex.ru>
2023-03-31 11:24:30 +0900

    Update App.java: Add thread pools

--- src/App.java
+++ src/App.java
@@ -1,14 +1,18 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 public class App {
     static String webSiteUrl = "https://zabgu.ru";
     static String newsPageUrl = "/php/news.php?category=1&page=";
-    static String previewsDir = "previews";
+    static String previewsDir = "previews2";
     static List<String[]> dataLines = new ArrayList<>();
-    static int pageCount = 1;
-    static String filename = "data.csv";
+    static int pageCount = 12;
+    static String filename = "data2.csv";
     static String searchStr = "ЗабГУ";
+    static int threadCount = 6;
 
     public static void main(String[] args) throws Exception {
         if (args.length != 0) {
@@ -28,25 +32,37 @@ public class App {
         }
     }
 
-    public static void newsParse() {
-        try {
-            NewsParser newsParser = new NewsParser(webSiteUrl, newsPageUrl, previewsDir);
-        
-            for (int i = 0; i < pageCount; i++) {
-                dataLines.addAll(newsParser.parseNewsPage(i + 1));
+    public static void newsParse() throws Exception {
+        NewsParser newsParser = new NewsParser(webSiteUrl, newsPageUrl, previewsDir);
+        ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(threadCount);
+    
+        for (int i = 0; i < pageCount; i++) {
+            final int ii = i;
+            executor.submit(() -> {
+                try {
+                    dataLines.addAll(newsParser.parseNewsPage(ii + 1));
+                            // очистка консоли
+                    System.out.print("\033[H\033[2J");
 
-                // очистка консоли
-                System.out.print("\033[H\033[2J");
-
-                System.out.print("Спарсено " + (i + 1) + " из " + pageCount + " страниц");
-            }
-            
-            CsvWriter csvWriter = new CsvWriter();
-            csvWriter.write(dataLines, filename);
-
-        } catch (Exception exception) {
-            System.out.println(exception.getMessage());
+                    System.out.print("Спарсено " + (ii + 1) + " из " + pageCount + " страниц");
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            });
         }
+        executor.shutdown();
+
+        // блокировка основного потока
+        while (true) {
+            boolean result_ = executor.awaitTermination(1, TimeUnit.DAYS);
+
+            if(result_)
+                break;
+        }
+
+        // этот код выполнится после завершения всех созданных ранее потоков
+        CsvWriter csvWriter = new CsvWriter();
+        csvWriter.write(dataLines, filename);
     }
 
     public static void countMatchesFromCsv()  {
